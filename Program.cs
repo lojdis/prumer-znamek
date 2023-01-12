eff54ff226b892d89184939dbb098665a2a33444
lojdis <113169810+lojdis@users.noreply.github.com>
2023-01-12 10:42:05 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace prumer_znamek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Z kolika známek chceeč počítat průměr?");
+            int pocet_znamek = int.Parse(Console.ReadLine());
+            int[] znamky = new int[pocet_znamek];
+            for (int posun_indexu = 0; posun_indexu < pocet_znamek; posun_indexu++)
+            {
+                Console.WriteLine("Zadejte {0}. známku", posun_indexu + 1);
+                znamky[posun_indexu] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Průměr tvých známek je:{0}", znamky.Min());
+            Console.ReadKey();
+        }
+    }
+}
